7089935c4bba2e79c479138b3f06c22099b5e86d
Wasif Mahmood <wasifmahmood2007@gmail.com>
2025-09-20 14:35:43 +0500

    Update Main.java

--- Object Oriented Classes/Point Class/Main.java
+++ Object Oriented Classes/Point Class/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-        //Using Default Constructor to create Custom Line
+        //Using Default Constructor to create Custom Point
         Point p1 = new Point();
         p1.setColor("Green");
         p1.setThickness(4);
@@ -9,7 +9,7 @@ public class Main {
         p1.setYCord(12);
         System.out.println(p1.show());
 
-        //Using Parameterized Constructor to create Custom Line
+        //Using Parameterized Constructor to create Custom Point
         Point p2 = new Point("Red", 2, -4, 6);
         System.out.println(p2.show());
 
@@ -17,4 +17,5 @@ public class Main {
         Point p3 = new Point();
         System.out.println(p3.show());
     }
+
 }
