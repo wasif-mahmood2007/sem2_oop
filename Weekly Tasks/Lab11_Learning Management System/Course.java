def091e39f889bfe5bfd47235ed311bd06eeef4d
Wasif Mahmood <wasifmahmood2007@gmail.com>
2025-10-03 12:37:59 +0500

    Update Lab11/Course.java

--- Weekly Tasks/Lab11_Learning Management System/Course.java
+++ Weekly Tasks/Lab11_Learning Management System/Course.java
@@ -1,12 +1,12 @@
-public class Course {
+public class Course implements Cloneable {
     private String courseName;
     private String code;
 
     //Default Constructor
     public Course()
     {
-        this.courseName="Unknown";
-        this.code="Untitled";
+        this.courseName="Untitled";
+        this.code="Unknown";
     }
 
     //Parameterized Constructor
@@ -23,26 +23,44 @@ public class Course {
         this.code=other.code;
     }
 
-    //Setters and Getters
+    //Setters
     public void setCourseName(String courseName)
     {
         this.courseName=courseName;
     }
+    
+    public void setCode(String code)
+    {
+        this.code=code;
+    }
+
+    //Getters
     public String getCourseName()
     {
         return courseName;
     }
 
-    public void setCode(String code)
-    {
-        this.code=code;
-    }
     public String getCode()
     {
         return code;
     }
 
-    //Print
+    //Override clone() for Shallow Copy
+    @Override
+    public Course clone()
+    {
+        try
+        {
+            Course shallowCopy = (Course) super.clone();
+            return shallowCopy;
+        }
+        catch (CloneNotSupportedException e)
+        {
+            throw new AssertionError();
+        }
+    }
+
+    //Override toString() to represent course
     @Override
     public String toString()
     {
